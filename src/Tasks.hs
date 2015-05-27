213d0c555a02a97dc608f001abc800f4d555ecb7
Gatlin Johnson <gatlin@niltag.net>
2015-05-27 14:06:45 -0500

    making sure the tests are being done right

--- src/Tasks.hs
+++ src/Tasks.hs
@@ -14,14 +14,18 @@ import Data.IORef
 
 import FreeStream
 
-readChunks :: Handle -> Source T.Text IO ()
-readChunks hndl = loop where
+-- | Emits chunks of 'T.Text' values from the supplied file handle, and
+-- terminates with 'T.empty'.
+readText :: Handle -> Source T.Text IO ()
+--          ^ IO handle to read from
+readText hndl = loop where
     loop = do
         ch <- lift $! T.hGetChunk hndl
         if T.null ch
             then yield ch >> return ()
             else yield ch >> loop
 
+-- | Breaks a 'T.Text' value into a stream of 'Char's.
 toChars :: Task T.Text Char IO ()
 toChars = do
     ch <- await
@@ -31,7 +35,10 @@ toChars = do
             each $ T.unpack ch
             toChars
 
+-- | Groups a stream of 'Char's into a 'T.Text' string based on the supplied
+-- predicate
 groupBy :: (Char -> Bool) -> Task Char T.Text IO ()
+--         ^ predicate function. Values giving @True@ are the separators
 groupBy pred = loop T.empty where
     loop acc = do
         ch <- await
@@ -42,16 +49,20 @@ groupBy pred = loop T.empty where
                 loop T.empty
             else loop $ T.cons ch acc
 
+-- | Convenience function to group a stream of 'Char's along whitespace
+-- boundaries
 tokenize :: Task Char T.Text IO ()
 tokenize = groupBy isSpace
 {-# INLINE tokenize #-}
 
+-- | Counts how many elements it has seen, storing the result in an 'IORef'.
 count :: IORef Int -> Sink a IO ()
 count r = forever $! do
     it <- await
     lift $ modifyIORef' r $ \n -> n + 1
 
-bigrams :: Task T.Text (T.Text, T.Text) IO ()
+-- | Converts a stream of arbitrary values into a stream of bigrams
+bigrams :: Task a (a, a) IO ()
 bigrams = do
     first <- await
     loop first
@@ -61,12 +72,12 @@ bigrams = do
             yield (prev, curr)
             loop curr
 
-writeChunks :: Handle -> Sink T.Text IO ()
-writeChunks hndl = loop where
+-- | Writes 'T.Text' values to the supplied file handle.
+writeText :: Handle -> Sink T.Text IO ()
+writeText hndl = loop where
     loop = do
         ch <- await
         if T.null ch
             then return ()
             else do
                 lift $ T.hPutStr hndl ch
-                loop
--- test/Test.hs
+++ test/Test.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, Rank2Types #-}
 
 import Tasks
 import Criterion.Main
@@ -15,7 +15,7 @@ brown_bigrams :: IO Int
 brown_bigrams = do
     n <- newIORef 0
     withFile "brown.txt" ReadMode $ \hndl -> do
-        run $ readChunks hndl
+        run $ readText hndl
            >< toChars
            >< tokenize
            >< bigrams
@@ -30,9 +30,9 @@ freestreamTest n = run $ each [1..n] >< map (+1) >< filter even >< cat
 main :: IO ()
 main = defaultMain
     [ bgroup "IO"
-        [ bench "bigrams" $ whnfIO brown_bigrams  ]
+        [ bench "bigrams" $ whnfIO brown_bigrams
+        , bench "freestream" $ whnf (runIdentity . freestreamTest) 10000 ]
 
     , bgroup "Identity"
-        [ bench "freestream" $ whnf (runIdentity . freestreamTest) 10000 ]
-    ]
+        [ bench "freestream" $ whnf (runIdentity . freestreamTest) 10000 ] ]
 
