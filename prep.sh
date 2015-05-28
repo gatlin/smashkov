e459e670fa15b18331cb9de7e78325414423d6e7
Gatlin Johnson <gatlin@niltag.net>
2015-05-28 16:10:01 -0500

    never used submodules before, hold on to your butts

--- prep.sh
+++ prep.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-git submodule update --init --recursive
-
 cabal sandbox init
-
+git submodule update --init --recursive
 cabal sandbox add-source thirdparty/FreeStream/
-
 cabal install --only-dependencies
-
 cabal configure
-
 cabal build
