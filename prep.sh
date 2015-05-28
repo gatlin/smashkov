70b1229fd9ef50410a60c36fd3fe713c7000146d
Gatlin Johnson <gatlin@niltag.net>
2015-05-28 16:27:31 -0500

    so many typos

--- prep.sh
+++ prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cabal sandbox init
-git clone https://github.com/gatlin/FreeStream.git
+git clone https://github.com/gatlin/FreeStream.git thirdparty/FreeStream
 cabal sandbox add-source thirdparty/FreeStream/
 cabal install --only-dependencies
 cabal configure
