b7f80f03302fbd7e25b2d863cdbc612c95d6b56c
Gatlin Johnson <gatlin@niltag.net>
2015-05-30 11:27:11 -0500

    added min and max sentence length options, changed default min

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE RecordWildCards #-}
 
 {-
 This module is just the 'main' function and some boring administrative code.
@@ -31,6 +32,8 @@ import Options.Applicative
 data CmdOpts = CmdOpts
     { _file :: String
     , _genHowMany :: String
+    , _maxLen :: String
+    , _minLen :: String
     }
 
 opts :: ParserInfo CmdOpts
@@ -45,20 +48,32 @@ opts = info (parser <**> helper) (fullDesc <> progDesc d)
                     (   long "howmany"
                     <>  short 'n'
                     <>  value "5")
+            <*> strOption
+                    (   long "max"
+                    <>  value "20" )
+            <*> strOption
+                    (   long "min"
+                    <>  value "10" )
+
         d = "a shitty tool for making shitty markov models"
 
 -- * Somebody once told me ...
 
-main :: IO ()
-main = do
-    opts' <- execParser opts
+start :: CmdOpts -> IO ()
+start CmdOpts{..} = do
     -- open the file and read the bigrams into the appropriate structure
-    mp <- withFile (_file opts') ReadMode $ \hndl -> do
+    mp <- withFile _file ReadMode $ \hndl -> do
         mp <- readBigrams hndl
         return mp
 
+    let min = read _minLen
+    let max = read _maxLen
+    let hm  = read _genHowMany
     -- now, generate sentences of random length and write them to stdout
-    forM_ [1..(read $ _genHowMany opts')] $ \_ -> do
-        howMany <- evalRandIO $ getRandomR (4, 20)
+    forM_ [1..hm] $ \_ -> do
+        howMany <- evalRandIO $ getRandomR (min, max)
         run $ walk mp >< take howMany >< map (`T.snoc` ' ') >< writeText stdout
         putStrLn "\n---"
+
+main :: IO ()
+main = execParser opts >>= start
